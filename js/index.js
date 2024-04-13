2234d35a304e10c3a29a27593193e2c3739e2afa
lizwachira <lizwanguwachira@gmail.com>
2024-04-13 06:44:18 +0300

    backgoundupdate

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-# GitHub Search App
+ //had to create a db.json on my terminal lets see..
+//something is not working here,sorted..
+//fetch and display the user's repositories
+//from thunder client I copy this list @client status 200 live to db.json
+//Liz gets the response on git api(https://api.github.com)
 
-//This IS a tool to search for users on github using Javascript.
+//on my web page i Liz was able to search my username and got other 2users
+
 const form = document.getElementById('github-form');
 const userList = document.getElementById('user-list');
 const reposList = document.getElementById('repos-list');
